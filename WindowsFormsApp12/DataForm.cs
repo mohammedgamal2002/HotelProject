7542031713ab46961d2062c126500a2d7842fedf
moken <moken@LAPTOP-IH3NAIAC>
2022-03-31 20:50:13 +0200

    changes on DataForm

--- WindowsFormsApp12/DataForm.cs
+++ WindowsFormsApp12/DataForm.cs
@@ -10,12 +10,36 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp12
 {
+    public class Customer
+    {
+        public string name { get; set; }
+        public string address { get; set; }
+        public string phoneNum { get; set; }
+        public string roomNum { get; set; } 
+        public Customer(string name, string address, string phoneNum, string roomNum) {
+            this.name = name;
+            this.address = address;
+            this.phoneNum = phoneNum;
+            this.roomNum = roomNum;
+        }
+
+    }
     public partial class DataForm : Form
     {
+        List<Customer> customersList = new List<Customer>(); // Customers File Should read into this list
         public DataForm()
         {
             InitializeComponent();
-            
+            customersList.Add(new Customer("Ahmed", "23 St asdas", "010321030", "22"));             //test case
+            customersList.Add(new Customer("mohammed", "23 St asdas", "010321030", "62"));            //test case
+
+            foreach (Customer customer in customersList)
+            {
+                dataGridView1.Rows.Add(customer.name,customer.address , customer.phoneNum , customer.roomNum);
+            }
         }
     }
+
+
+
 }
